2f951df7d69449b906abd47eac84f9f69f80e6c8
Lukas <lukas@kjsoft.com.tw>
2018-07-10 14:56:57 +0800

    完成CRUD+dropdownlist

---
+++ MVCtest20180709/Controllers/usersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCtest20180709.Models;
+
+namespace MVCtest20180709.Controllers
+{
+    public class usersController : Controller
+    {
+        private LukasEntities db = new LukasEntities();
+
+        // GET: users
+        public ActionResult Index()
+        {
+            var user = db.user.Include(u => u.dept);
+            return View(user.ToList());
+        }
+
+        // GET: users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: users/Create
+        public ActionResult Create()
+        {
+            ViewBag.dept_id = new SelectList(db.dept, "dept_id", "dept_name");
+            return View();
+        }
+
+        // POST: users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,user_name,dept_id,update_date")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.dept_id = new SelectList(db.dept, "dept_id", "dept_name", user.dept_id);
+            return View(user);
+        }
+
+        // GET: users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.dept_id = new SelectList(db.dept, "dept_id", "dept_name", user.dept_id);
+            return View(user);
+        }
+
+        // POST: users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "user_id,user_name,dept_id,update_date")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.dept_id = new SelectList(db.dept, "dept_id", "dept_name", user.dept_id);
+            return View(user);
+        }
+
+        // GET: users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user user = db.user.Find(id);
+            db.user.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
